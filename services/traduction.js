4a7b128b8c43c3ba6b86b8dd5af54548152c0b71
Yoann Picquenot <yoann.picquenot@gmail.com>
2017-01-20 01:44:24 +0000

    Add home traduction

---
+++ services/traduction.js
@@ -0,0 +1,90 @@
+var app = angular.module("umovie-app");
+app.run([
+	"$rootScope",
+	"$cookies",
+	function($rootScope, $cookies) {
+		var lang = $cookies.get("lang");
+		if(lang == null) {
+			$cookies.put("lang", "en");
+		}
+
+		var dictionnary = {
+			"bannersubtitle": {
+                "fr": "Regardez autrement.",
+                "en": "Watch different."
+            },
+            "follow-block-title": {
+                "fr": "Suivez",
+                "en": "Follow"
+            },
+            "follow-block-text": {
+                "fr": "Ne ratez jamais vos séries favorites grâce à vos watchlists",
+                "en": "Don't miss any series of yours thanks to your Watchlists"
+            },
+            "home": {
+                "fr": "Accueil",
+                "en": "Home"
+            },
+            "footer-information": {
+                "fr": "Informations",
+                "en": "Information"
+            },
+            "footer-information-text": {
+                "fr": "Toutes les données proviennents de Apple Inc. Ce projet a été créé dans un cadre pédagogique.",
+                "en": "Every data are provided by Apple Inc. This project has been created within a pedagogical aim."
+            },
+            "footer-contact-1": {
+                "fr": "Pigeon voyageur",
+                "en": "Carrier piegon"
+            },
+            "footer-contact-2": {
+                "fr": "Ligne 800 : arrêt Université Laval, pavillon Pouliot (On est souvent a l'ASETIN, demandez pour trouver)",
+                "en": "Bus 800 : bus stop Laval University, Pouliot block (We are often at ASETIN office, ask for it)"
+            },
+            "footer-contact-text": {
+                "fr": "Pour nous contacter",
+                "en": "To contact us"
+            },
+            "logout": {
+                "fr": "Se déconnecter",
+                "en": "Log out"
+            },
+            "presentationtitle": {
+                "fr": "En résumé...",
+                "en": "In summary..."
+            },
+            "share-block-title": {
+                "fr": "Share",
+                "en": "Share"
+            },
+            "share-block-text": {
+                "fr": "Partagez vos watchlists avec vos amis",
+                "en": "Share your watchlists with your friends"
+            },
+            "search": {
+                "fr": "Rechercher",
+                "en": "Search"
+            },
+            "settings": {
+                "fr": "Paramètres",
+                "en": "Settings"
+            },
+            "watch-block-title": {
+                "fr": "Regardez",
+                "en": "Watch"
+            },
+            "watch-block-text": {
+                "fr": "Visionnez vos films et vos séries préférées",
+                "en": "Watch your favorite movies and series"
+            },
+            "watchlist": {
+                "fr": "Watchlist",
+                "en": "Watchlist"
+            },
+        }
+
+        $rootScope.translate = function (expr) {
+    		return dictionnary[expr][lang];
+        };
+	}
+]);
